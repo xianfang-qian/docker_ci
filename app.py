852d78e3f4d83b0d99ea3772bbe46149d48f617f
xianfang-qian <63028324+xianfang-qian@users.noreply.github.com>
2021-12-06 11:28:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """Auxiliary authentification details for Snyk security scanning"""
+RUN mkdir -p /data/docker_ci
 import os
 
 from utils.utilities import check_printable_utf8_chars
@@ -8,3 +9,10 @@ from utils.utilities import check_printable_utf8_chars
 SNYK_API = check_printable_utf8_chars(os.getenv('SNYK_API', 'https://snyk.io/api/v1/'))
 # Snyk API token, obtained from https://app.snyk.io/account to run SDL tests
 SNYK_TOKEN = check_printable_utf8_chars(os.getenv('SNYK_TOKEN', ''))
+
+
+echo "Show files in /app"
+ls
+echo "Show files in /data/docker_ci"
+cd /data/docker_ci
+ls
